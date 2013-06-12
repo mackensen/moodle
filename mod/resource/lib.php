17652c04d54a25fe044f5f91af114d4ac7f03afb
Daniel Landau <nprolog@postalmodern.com>
2013-06-12 11:28:49 -0700

    CLAMP-457 course: set default display view for resources

--- mod/resource/lib.php
+++ mod/resource/lib.php
@@ -506,7 +506,7 @@ function resource_dndupload_handle($uploadinfo) {
 
     // Set the display options to the site defaults.
     $config = get_config('resource');
-    $data->display = $config->display;
+    $data->display = $uploadinfo->course->filedisplaydefault;
     $data->popupheight = $config->popupheight;
     $data->popupwidth = $config->popupwidth;
     $data->printintro = $config->printintro;
