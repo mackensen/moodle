179395b5b5938befceda3ef2144814b887a9a709
Charles Fulton <mackensen@gmail.com>
2014-04-16 13:15:36 -0700

    CLAMP-654 phpunit: increase expected number of users

--- lib/phpunit/tests/advanced_test.php
+++ lib/phpunit/tests/advanced_test.php
@@ -146,9 +146,9 @@ class core_phpunit_advanced_testcase extends advanced_testcase {
         $course = $this->getDataGenerator()->create_course();
         $this->assertEquals(2, $course->id);
 
-        $this->assertEquals(2, $DB->count_records('user'));
+        $this->assertEquals(3, $DB->count_records('user'));
         $DB->delete_records('user', array('id'=>1));
-        $this->assertEquals(1, $DB->count_records('user'));
+        $this->assertEquals(2, $DB->count_records('user'));
 
         //=========
 
@@ -162,10 +162,10 @@ class core_phpunit_advanced_testcase extends advanced_testcase {
         $displayid = $DB->insert_record('user_preferences', array('userid'=>2, 'name'=> 'phpunittest', 'value'=>'x'));
         $this->assertEquals($originaldisplayid, $displayid);
 
-        $this->assertEquals(2, $DB->count_records('user'));
+        $this->assertEquals(3, $DB->count_records('user'));
         $DB->delete_records('user', array('id'=>2));
         $user = $this->getDataGenerator()->create_user();
-        $this->assertEquals(3, $user->id);
+        $this->assertEquals(4, $user->id);
 
         // =========
 
@@ -174,7 +174,7 @@ class core_phpunit_advanced_testcase extends advanced_testcase {
         $course = $this->getDataGenerator()->create_course();
         $this->assertEquals(2, $course->id);
 
-        $this->assertEquals(2, $DB->count_records('user'));
+        $this->assertEquals(3, $DB->count_records('user'));
         $DB->delete_records('user', array('id'=>2));
 
         //==========
@@ -184,7 +184,7 @@ class core_phpunit_advanced_testcase extends advanced_testcase {
         $course = $this->getDataGenerator()->create_course();
         $this->assertEquals(2, $course->id);
 
-        $this->assertEquals(2, $DB->count_records('user'));
+        $this->assertEquals(3, $DB->count_records('user'));
     }
 
     public function test_change_detection() {
